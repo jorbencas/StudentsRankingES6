db99222bee9d7fb6da2ab06ed70a4c253410e558
jorbencas <jorbencas@gmil.com>
2017-11-07 13:40:13 +0100

    Adding router, map and crud

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,48 @@
-var fs = require("fs");
-var browserify = require("browserify");
+var fs = require('fs');
+var browserify = require('browserify');
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
-var jslint = require('gulp-jslint-simple');
-const jscs = require('gulp-jscs');
+var $ = require('gulp-load-plugins')({ lazy: true });
+var jsdoc = require('gulp-jsdoc3');
+//var jshint = require('gulp-jshint');
 
-if (!fs.existsSync("dist")){
-  fs.mkdirSync("dist");
+if (!fs.existsSync('dist')){
+  fs.mkdirSync('dist');
 }
+
+/**
+ * vet the code and create coverage report
+ * @return {Stream}
+ */
+gulp.task('vet', function() {
+  //log('Analyzing source with JSHint and JSCS');
+  return gulp
+    .src('./src/*')
+    //.pipe($.if(args.verbose, $.print()))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
+    .pipe($.jshint.reporter('fail'))
+    .pipe($.jscs())
+    .pipe($.jscs.reporter());
+});
+
+/**
+ * $ gulp
+ * description: Generate automatically all development documentation using jsdoc
+ */
+
+gulp.task('doc', function (cb) {
+  var config = require('./jsdoc.json');
+  gulp.src(['README.md','./src/**/*.js'], {read: false})
+    .pipe(jsdoc(config, cb));
+});
 
 ///babelify, es6 to es5
 gulp.task('browserify', function() {
-browserify("./src/main.js")
-  .transform("babelify", {presets: ["es2015"]})
+  browserify('./src/main.js')
+  .transform('babelify', {presets: ['es2015']})
   .bundle()
-  .pipe(fs.createWriteStream("dist/main.js"));
+  .pipe(fs.createWriteStream('dist/main.js'));
 });
 
 ///http server live reload (html changes)
@@ -27,27 +55,8 @@ gulp.task('webserver', function() {
   }));
 });
 
-gulp.task('lint', function () {
-  gulp.src('./src/*.js')
-      .pipe(jslint.run({
-          // project-wide JSLint options
-          node: true,
-          vars: true
-      }))
-      .pipe(jslint.report({
-          // example of using a JSHint reporter
-          reporter: require('jshint-stylish').reporter
-      }));
-});
-
-gulp.task('jscs', () => {
-  return gulp.src('./src/*.js')
-      .pipe(jscs())
-      .pipe(jscs.reporter());
-});
-
 // watch any change
 gulp.task('watch', ['browserify'], function () {
-    gulp.watch('./src/**/*.js', ['browserify']);
+  gulp.watch('./src/**/*.js', ['browserify']);
 });
 gulp.task('default', ['browserify', 'webserver', 'watch']);
--- src/context.js
+++ src/context.js
@@ -4,77 +4,163 @@
  * @constructor
  * @tutorial pointing-criteria
  */
+
+/*jshint -W061 */
+
 import Person from './person.js';
 import GradedTask from './gradedtask.js';
-import {hashcode,getElementTd} from './utils.js';
+import {hashcode,getElementTd,deleteContent,loadTemplate} from './utils.js';
 
 class Context {
-
-  constructor() {
-    this.students = [
-        new Person("Paco", "Vañó", 5),
-        new Person("Lucia", "Botella", 10),
-        new Person("German", "Ojeda", 3),
-        new Person("Salva", "Peris", 1),
-        new Person("Oscar", "Carrion", 40)
-    ]; 
-    
-  }
-  adTask(){
-    var addTask = document.getElementById("addGradedTask");
-    addTask.addEventListener("click", () => {
-          this.addGradedTask();
-    });
-    this.gradedTasks = [];
-  }
-  addStudent(){ 
-      var addStudents = document.getElementById("addStudents");
-      addStudents.addEventListener("click", () => {
-          this.addStudents();
-      });
-  }
-  /** Draw Students rank table in descendent order using points as a criteria */
-  getRanking(){
-        this.students.sort(function(a, b) {
-            return (b.points - a.points);
-        });
-        var studentsEl = document.getElementById("llistat");
-   
-        while (studentsEl.firstChild) {
-            studentsEl.removeChild(studentsEl.firstChild);
+  
+    constructor() {
+      this.students = new Map();
+      this.gradedTasks = [];
+      this.showNumGradedTasks = 1;
+  
+      if (localStorage.getItem('students')) {//save the student in localstorage
+        let students_ = new Map(JSON.parse(localStorage.getItem('students')));
+        
+        for (var [key, value] of students_) {
+        
+         var value = JSON.stringify(value);
+         var value = JSON.parse(value);
+        
+         let p = new Person(value.name,value.surname, value.attitudeTasks,value.gradedTasks);
+         this.students.set(key, p);
+        
         }
 
-        let headerString="<tr><td colspan='3'></td>";
-        this.gradedTasks.forEach(function(taskItem){            
-            headerString+="<td>"+taskItem.name+"</td>";
-        });
-        studentsEl.innerHTML= headerString;
-        this.students.forEach(function(studentItem) {
-            var liEl = studentItem.getHTMLView();
-            studentsEl.appendChild(liEl);
-        });
+      }
+      if (localStorage.getItem('gradedTasks')) {
+        this.gradedTasks = JSON.parse(localStorage.getItem('gradedTasks'));
+      }
     }
+  
+    /** Draw Students rank table in descendent order using points as a criteria */
+    getTemplateRanking() {
+      if (this.students && this.students.size > 0) {
+       
+        var stud = new Map([...this.students.entries()].sort((a,b) => a[1].getTotalPoints() < b[1].getTotalPoints()));
+       
+        localStorage.setItem('students',JSON.stringify([...stud]));
+  
+        let GRADED_TASKS = '';
+        this.gradedTasks.forEach(function(taskItem) {
+          GRADED_TASKS += '<td>' + taskItem.name + '</td>';
+        });
+  
+        loadTemplate('templates/rankingList.html',function(responseText) {
+                document.getElementById('content').innerHTML = eval('`' + responseText + '`');
+                let tableBody = document.getElementById('idTableRankingBody');
+                stud.forEach(function(studentItem) {
+                 // console.log(studentItem);
+                  let liEl = studentItem.getHTMLView();
+                  tableBody.appendChild(liEl);
+                });
+              }.bind(this));
+      }else{
+        alert('Please creat some student please');
+        // loadTemplate('templates/rankingList.html',function() {
+        //   let content = document.getElementById('content');
+        //    let err = document.createElement('p');
+        //     let text = document.createTextNode('Please creat some student please');
+        //     err.appendChild(text);
+        //     content.appendChild(err);
+        // }.bind(this));
+      }//end else 
+    }
+  
     /** Create a form to create a GradedTask that will be added to every student */
-   addGradedTask(){        
-        let taskName = prompt("Please enter your task name");
-        let gtask = new GradedTask(taskName);
-        this.gradedTasks.push(gtask);
-        this.students.forEach(function(studentItem) {            
-            studentItem.addGradedTask(gtask);
-        });
-        this.getRanking();
-        
+    addGradedTask() {
+  
+      let callback = function(responseText) {
+              let saveGradedTask = document.getElementById('newGradedTask');
+  
+              saveGradedTask.addEventListener('submit', () => {
+                let name = document.getElementById('idTaskName').value;
+                let description = document.getElementById('idTaskDescription').value;
+                let weight = document.getElementById('idTaskWeight').value;
+                let gtask = new GradedTask(name,description,weight);
+                this.gradedTasks.push(gtask);
+                localStorage.setItem('gradedTasks',JSON.stringify(this.gradedTasks));
+                this.students.forEach(function(studentItem) {
+                  studentItem.addGradedTask(gtask);
+                });
+                this.getTemplateRanking();
+              });
+            }.bind(this);
+  
+      loadTemplate('templates/addGradedTask.html',callback);
     }
-    addStudents(){
-        let studentName = prompt("Enter the student name");
-        let adds = new Person(studentName,"Martinez",0);
-        this.students.push(adds);
-        this.gradedTasks.forEach(function(tasks){
-            adds.addGradedTask(tasks);
-        });
-        this.getRanking();
-        
-    }
-}
+    /** Add a new person to the context app */
+    addPerson() {
+  
+      let callback = function(responseText) {
+              let saveStudent = document.getElementById('newStudent');
 
-export default Context = new Context();
+              saveStudent.addEventListener('submit', () => {
+                let name = document.getElementById('idFirstName').value;
+                let surnames = document.getElementById('idSurnames').value;
+                let student = new Person(name,surnames,[],[]);
+                this.gradedTasks.forEach(function(iGradedTask) {
+                      student.addGradedTask(new GradedTask(iGradedTask.name));
+                    });
+
+                this.students.set(hashcode(student.name + student.surname), student);
+              
+                localStorage.setItem('students',JSON.stringify([...this.students]));
+              });
+            }.bind(this);
+  
+      loadTemplate('templates/addStudent.html',callback);
+    }
+    /** Add last action performed to lower information layer in main app */
+  
+    notify(text) {
+      document.getElementById('notify').innerHTML = text;
+    }
+
+    //delete one student
+  deletestudent(hash){
+    for (var [key, value]  of this.students) {
+    let hesh = parseInt(hash);
+       if (key === hesh) {//copare the hash in localstorega with get hash
+       if (confirm('Are you sure to delete student ' + value.name + ' ' + value.surname + ' ? ')) {
+        this.students.delete(key);
+       }
+     }
+    }
+    this.getTemplateRanking();
+  }
+/**update one student/ */
+  updatestudent(hash){
+    for (var [key, value]  of this.students) {
+      let hesh = parseInt(hash);
+     if (key === hesh) {
+      let that = key;
+      let thatval = value;
+     let callback = function (responseText){ 
+           document.getElementById('idFirstName').value = thatval.name;//insert real values in form 
+           document.getElementById('idSurnames').value = thatval.surname;//insert real  values in form 
+         let saveStudent = document.getElementById('newStudent');
+          saveStudent.addEventListener('submit', () => {
+            context.students.delete(that);//deete the student
+           let name = document.getElementById('idFirstName').value;
+           let surnames = document.getElementById('idSurnames').value;
+           let student =  new Person(name,surnames,[],[]);
+           context.gradedTasks.forEach(function(iGradedTask) {
+            student.addGradedTask(new GradedTask(iGradedTask.name));
+          });
+           context.students.set(hashcode(student.name + student.surname), student);   
+           localStorage.setItem('students',JSON.stringify([...context.students]));
+         });
+      }.bind(this.that);
+      loadTemplate('templates/updateStudent.html',callback);
+     }
+    }
+  }
+
+}
+  
+  export let context = new Context(); //Singleton export
